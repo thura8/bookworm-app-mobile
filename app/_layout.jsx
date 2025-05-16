6f697041098a0274330bcdb5ff3e9b46e072b8e4
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-05-16 11:51:47 +0630

    Root layout setup

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+
+import SafeScreen from "@/components/SafeScreen";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(auth)" />
+        </Stack>
+      </SafeScreen>
+      <StatusBar style="auto" />
+    </SafeAreaProvider>
+  );
 }
